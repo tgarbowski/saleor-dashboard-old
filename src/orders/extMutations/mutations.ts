cb09d692cde1884f8c56f63a082f209d40bfe24e
Tooomas <tomek.dominiczak@gmail.com>
2022-05-21 12:07:29 +0200

    Change receipt printing method to communicate with api

---
+++ src/orders/extMutations/mutations.ts
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const ExtReceiptRequestMutation = gql`
+  mutation ExtReceiptRequest($orderId: ID!) {
+    extReceiptRequest(orderId: $orderId) {
+      payload
+      invoice {
+        id
+      }
+    }
+  }
+`;
+
+export const ExtReceiptUpdateMutation = gql`
+  mutation ExtReceiptUpdate($id: ID!, $input: ExtReceiptInput!) {
+    extReceiptUpdate(id: $id, input: $input) {
+      errors {
+        message
+      }
+    }
+  }
+`;
